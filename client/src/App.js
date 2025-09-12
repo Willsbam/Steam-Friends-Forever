8ff77dbf23dd8dc7bfa3af70b0bc372ddd93d603
Willsbam <wlbramley@gmail.com>
2025-09-12 18:52:34 -0400

    Fixed the timing of the delve into friendlist message

--- client/src/App.js
+++ client/src/App.js
@@ -224,10 +224,8 @@ useEffect(() => {
         <h3><b>Loading New Profile...</b></h3>
       ) : proccessing ? (
         <h3><b>Loading Friends...</b></h3>
-      ) : mainUserData !== -1 && friendStack.length > 0 ? (
+      ) : (proccessing===false && isTransitioning===false && mainUserData !== -1 && friendStack.length > 0) ? (
         <h3><b>Click on any friend to delve into their friend chart</b></h3>
-      ) : mainUserData !== -1 ? (
-        <h3><b>Loading Friends...</b></h3>
       ) : null}
     </div>
 
